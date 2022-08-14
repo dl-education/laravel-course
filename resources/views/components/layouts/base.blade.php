e9180157e1c2374b75c1a537960f8685fdef7ccd
Vladimir Yurchenko <sibkedr55@gmail.com>
2022-08-14 22:36:17 +0600

    fix error Attempt to read property email_verified_at on null

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -29,38 +29,40 @@
                 <div class="col col-12 col-md-3">
                     <ul class="nav nav-pills flex-column mb-auto">
                         @auth
-                        <li class="nav-item">
-                            <a href="{{ route('home') }}" class="nav-link link-dark">Главная</a>
-                        </li>
-                        @can('admin-tags')
-                        <li class="nav-item">
-                            <a href="{{ route('tags.index') }}" class="nav-link link-dark">Теги</a>
-                        </li>
-                        @endif
-                        <li class="nav-item">
-                            <a href="{{ route('posts.index') }}" class="nav-link link-dark">Блог</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('videos.index') }}" class="nav-link link-dark">Видео</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('profile.password.edit') }}" class="nav-link link-dark">Смена пароля</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('address.form') }}" class="nav-link link-dark">Анализ адреса</a>
-                        </li>
+                            <li class="nav-item">
+                                <a href="{{ route('home') }}" class="nav-link link-dark">Главная</a>
+                            </li>
+                            @can('admin-tags')
+                            <li class="nav-item">
+                                <a href="{{ route('tags.index') }}" class="nav-link link-dark">Теги</a>
+                            </li>
+                            @endif
+                            <li class="nav-item">
+                                <a href="{{ route('posts.index') }}" class="nav-link link-dark">Блог</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('videos.index') }}" class="nav-link link-dark">Видео</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('profile.password.edit') }}" class="nav-link link-dark">Смена пароля</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('address.form') }}" class="nav-link link-dark">Анализ адреса</a>
+                            </li>
                         @else
-                        <li>
-                            <a href="{{ '/' }}" class="nav-link link-dark">Блог</a>
-                        </li>
+                            <li>
+                                <a href="{{ '/' }}" class="nav-link link-dark">Блог</a>
+                            </li>
                         @endif
                     </ul>
                 </div>
                 <main class="col col-12 col-md-9">
-                    @if(!auth()->user()->email_verified_at)
-                        <div class="alert alert-danger">Подтверди почту!!!!</div>
+                    @auth
+                        @if(!auth()->user()->email_verified_at)
+                            <div class="alert alert-danger">Подтверди почту!!!!</div>
+                        @endif
+                        <x-notifications />
                     @endif
-                    <x-notifications />
                     <h1 class="h3 mb-4">{{ $title }}</h1>
                     {{ $slot }}
                 </main>
